70879d6406f9365405d9e940f805cecc6ef61947
Gabriel <gabriel.cruz@maximatech.com.br>
2022-03-09 01:37:19 -0300

    first commit

---
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.todolist.R.layout.activity_main
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_main)
+    }
+}
